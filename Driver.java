1faf1c7bf7eb01caebaeeb8bd9785d755477b1f7
michellecdu <48156165+michellecdu@users.noreply.github.com>
2019-05-23 00:50:27 -0400

    Add files via upload

---
+++ Driver.java
@@ -0,0 +1,83 @@
+import java.util.concurrent.TimeUnit; // for the timers
+import javax.swing.*;
+import java.awt.event.*; // for the listener
+import java.awt.*;
+/******************************************************************
+* The Driver class contains the main method and runs the final game.
+* @authors Michelle Du, Shreya Singh
+*******************************************************************/
+public class Driver
+  {
+  /*****************************
+  * Main method (runs the game).
+  ******************************/
+  public static void main(String[] args) throws Exception
+   {
+      /**
+      * current time, used to figure out how long the song should play for
+      */
+      long start = System.currentTimeMillis();
+             
+      // create JFrame
+      JFrame frame = new JFrame("Frozen Dance Dance Revolution");
+      frame.setSize(600, 600);
+      frame.setLocation(0, 0);
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      
+      // create Scoreboard
+      Scoreboard scoreboard = new Scoreboard();
+      frame.setContentPane(scoreboard);
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      
+      // create start button and add to JFrame
+      JButton b=new JButton("START");  
+      frame.add(b, BorderLayout.NORTH);
+
+      
+      // create display and add to JFrame
+      Display m = new Display();
+      frame.add(m); 
+      frame.setVisible(true);
+      
+      b.addActionListener(new ActionListener()
+      {
+        /***********************************
+        * Plays song when button is pressed.
+        ************************************/
+        public void actionPerformed(ActionEvent e)
+        {
+            Sound.play("Frozen - Do You Want to Build a Snowman.wav"); // plays song
+            setMusicStarted(true); // tells rest of program the music has started
+        }
+      });    
+      b.setFocusable(false); // allows focus to go to the Display
+      boolean first = true; // tells scoreboard that this was the first key (scoring is different)
+      while (!musicStarted)
+      {
+         TimeUnit.MILLISECONDS.sleep(500); // wait for the music to start
+      }
+      
+      while (System.currentTimeMillis() - start < 750000) { // only play a portion of the song
+         long startTime = System.currentTimeMillis(); // keeps track of when the arrow started moving{
+         TimeUnit.MILLISECONDS.sleep(6800); // wait for arrow to get to the top of the screen
+         scoreboard.update(m, scoreboard.getScoreLabel(), first); // update score
+         first = false; // change first (for the remainder of the program)
+         m.setY(1000); // make the arrow go back to the bottom of the screen
+         m.changeArrow(); // change which arrow is displayed
+         m.timer.restart(); // restart timer
+      } 
+      //frame.repaint();
+      //ImageIcon gameover = new ImageIcon("images/gameover.jpeg");
+      System.exit(0); // exit JFrame after song is finished 
+      System.out.println("Congrats! Your final score was: " + scoreboard.getScore());  
+   }
+      /***********************************
+        * musicStarted returns whether or
+        * not the music has started.
+        ************************************/
+      private static boolean musicStarted = false; // used to determine if arrows should start moving
+      public static void setMusicStarted(boolean started)
+      {
+         musicStarted = started;
+      } 
+}
